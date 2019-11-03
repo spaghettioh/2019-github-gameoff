8b40ab35e969ea5c9d3e0c5f8f95fdbf988f52ec
Eric <ericmofield@gmail.com>
2019-11-03 02:35:57 -0500

    Add some health modifier scripts

---
+++ Assets/Scripts/HealthPickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider2D))]
+public class HealthPickup : MonoBehaviour
+{
+    public int healAmount = 1;
+    public RandomAudioPlayer collectionSFX;
+    public UnityEvent whenCollected;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == CharacterName.PlayerInstance.gameObject)
+        {
+            other.gameObject.GetComponent<Damageable>().GainHealth(healAmount);
+            whenCollected.Invoke();
+        }
+    }
+}
