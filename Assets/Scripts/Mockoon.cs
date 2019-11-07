f902c377cff05289e5955146e3741b63fdf6d967
Eric Mofield <ericmofield@gmail.com>
2019-11-07 01:41:44 -0500

    Add generic game level.

---
+++ Assets/Scripts/Mockoon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mockoon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MoveForward()
+    {
+
+    }
+
+    void Jump()
+    {
+
+    }
+
+    void HandleInput()
+    {
+
+    }
+}
