164493afd8e42f31a590a3780602e4bdf3723f4a
Eric Mofield <ericmofield@gmail.com>
2019-11-08 23:53:01 -0500

    Update level camera to automatically output to render texture (tv)

---
+++ Assets/Scripts/CameraSetter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraSetter : MonoBehaviour
+{
+    public RenderTexture gameCameraOutput;
+
+    /// <summary>
+    /// Checks to see if this camera's scene is loaded from the AlleyScene and
+    /// changes the display output to the render texture so it shows up on
+    /// the TV in the alley.
+    /// </summary>
+    void Awake()
+    {
+        if (SceneManager.GetActiveScene().name == "AlleyScene")
+            gameObject.GetComponent<Camera>().targetTexture = gameCameraOutput;
+    }
+}
