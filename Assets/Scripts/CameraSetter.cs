131290e4ad389549d3d2a750ceabcce83c5a97d0
Eric <ericmofield@gmail.com>
2019-11-09 01:11:33 -0500

    Update CameraSetter to also add an audio listener.

--- Assets/Scripts/CameraSetter.cs
+++ Assets/Scripts/CameraSetter.cs
@@ -6,13 +6,20 @@ public class CameraSetter : MonoBehaviour
     public RenderTexture gameCameraOutput;
 
     /// <summary>
-    /// Checks to see if this camera's scene is loaded from the AlleyScene and
-    /// changes the display output to the render texture so it shows up on
-    /// the TV in the alley.
+    /// Checks to see if this camera's scene is loaded from the AlleyScene.
+    /// If it is, change this camera's output to the render texture so that
+    /// it appears on the TV in the alley.
+    /// If not, add some stuff so that making levels works like normal.
     /// </summary>
     void Awake()
     {
         if (SceneManager.GetActiveScene().name == "AlleyScene")
+        {
             gameObject.GetComponent<Camera>().targetTexture = gameCameraOutput;
+        }
+        else
+        {
+            gameObject.AddComponent<AudioListener>();
+        }
     }
 }
