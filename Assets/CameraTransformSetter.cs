783de132bde43fa4306ad6c16c0e1bc3709c255a
Eric Mofield <ericmofield@gmail.com>
2019-11-15 00:15:51 -0500

    Create camera prefab, add camera script

---
+++ Assets/CameraTransformSetter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraTransformSetter : MonoBehaviour
+{
+    public TransformVariable thisCameraTransform;
+    public TransformVariable raccoonTransform;
+
+    private void Start()
+    {
+        gameObject.GetComponent<CinemachineVirtualCamera>().Follow = raccoonTransform.value;
+    }
+
+    void Update()
+    {
+        thisCameraTransform.SetValue(transform);
+    }
+}
