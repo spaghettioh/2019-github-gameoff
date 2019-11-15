d37ee3caa09a2696b5ec65f52a9d4efa3ac3e72c
Bradley Shively <bshively@gmail.com>
2019-11-14 23:27:09 -0500

    Implement basic level manager with levelindex scriptable object

---
+++ Assets/Scripts/LevelAdvancer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelAdvancer : MonoBehaviour
+{
+    public LevelIndex levelIndex;
+    public string[] levels;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelIndex.currentLevel = 0;
+        SceneManager.LoadSceneAsync(levels[levelIndex.currentLevel], LoadSceneMode.Additive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            NextLevel();
+        }
+    }
+
+    void NextLevel()
+    {
+        SceneManager.UnloadSceneAsync(levels[levelIndex.currentLevel]);
+        levelIndex.currentLevel += 1;
+        SceneManager.LoadSceneAsync(levels[levelIndex.currentLevel], LoadSceneMode.Additive);
+    }
+}
---
+++ Assets/Scripts/Variables/LevelIndex.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+[CreateAssetMenu]
+public class LevelIndex : ScriptableObject
+{
+    public int currentLevel = 0;
+
+}
