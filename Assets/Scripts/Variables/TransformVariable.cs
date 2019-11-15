7272e1db00f8c5497498482c92de447643015153
Eric Mofield <ericmofield@gmail.com>
2019-11-15 00:12:33 -0500

    Create Transform variable and assets

---
+++ Assets/Scripts/Variables/TransformVariable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class TransformVariable : ScriptableObject
+{
+    public Transform value;
+
+    public void SetValue(Transform t)
+    {
+        value = t;
+    }
+}
