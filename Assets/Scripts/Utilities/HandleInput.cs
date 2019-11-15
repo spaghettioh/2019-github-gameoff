7065a42ebcceba4d6529790343aa93c2d4d795e3
Eric Mofield <ericmofield@gmail.com>
2019-11-14 22:45:18 -0500

    Update title scene

---
+++ Assets/Scripts/Utilities/HandleInput.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+namespace Parcoun.Utilities
+{
+    public class HandleInput : MonoBehaviour
+    {
+        public UnityEvent anyKeyPressed;
+
+        void Update()
+        {
+            if (Input.anyKeyDown)
+            {
+                anyKeyPressed.Invoke();
+            }
+        }
+
+        public void LoadScene(string s)
+        {
+            SceneManager.LoadScene(s);
+        }
+    }
+}
