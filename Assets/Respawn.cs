8bb630a8061794f7eebae3e5b5b260b2a97ff291
Eric Mofield <ericmofield@gmail.com>
2019-11-17 16:18:06 -0500

    Add respawner script

---
+++ Assets/Respawn.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public Transform spawner;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Raccoon")
+            gameObject.transform.position = spawner.position;
+    }
+}
