0d60cd854a1b7a79f8d40bb2249d7412453199a8
Eric <ericmofield@gmail.com>
2019-11-17 19:06:07 -0500

    Organize scripts

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public string thisSceneName;
+
+    private void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "AlleyScene")
+        {
+            LoadASceneAsync("Title");
+        }
+    }
+    public void LoadASceneAsync(string sceneName)
+    {
+        print(SceneManager.GetAllScenes());
+        SceneManager.UnloadSceneAsync(thisSceneName); ;
+        SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+    }
+}
