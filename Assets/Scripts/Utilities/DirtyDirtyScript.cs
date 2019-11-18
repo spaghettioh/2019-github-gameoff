e421fd7c8cdf123e9859f89e08e92dacbe1638f2
Eric <ericmofield@gmail.com>
2019-11-17 21:05:26 -0500

    Zoom TV with a dirty timeline hack

---
+++ Assets/Scripts/Utilities/DirtyDirtyScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+public class DirtyDirtyScript : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject.Find("Alley timeline").GetComponent<PlayableDirector>().Play();
+    }
+}
---
+++ Assets/Scripts/Utilities/GameEvent.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEvent : MonoBehaviour
+{
+    public UnityEvent onStart;
+    [Space]
+    [Space]
+    [Space]
+    public UnityEvent onUpdate;
+    [Space]
+    [Space]
+    [Space]
+    public UnityEvent onFixedUpdate;
+    [Space]
+    [Space]
+    [Space]
+    public UnityEvent onManuallyFire;
+
+    void Start()
+    {
+        onStart.Invoke();
+    }
+
+    void Update()
+    {
+        onUpdate.Invoke();
+    }
+
+    void FixedUpdate()
+    {
+        onFixedUpdate.Invoke();
+    }
+
+    public void ManuallyFire()
+    {
+        onManuallyFire.Invoke();
+    }
+}
